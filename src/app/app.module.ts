ff88b39ab3657120f7d1592edd2c7a3e497ac818
yuukse <sh4dow.safe@gmail.com>
2024-01-09 15:44:46 -0300

    v1.3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,16 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import {MatMenuModule} from '@angular/material/menu';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
+    MatMenuModule,
     MatCardModule,
     MatButtonModule,
     MatIconModule,
